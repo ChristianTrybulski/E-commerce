fec668cccb81ed0d55de8e715f1b8fca598441f3
CHRISTIAN TRYBULSKI <christian.trybulski@gmail.com>
2022-09-20 00:28:16 -0300

    PreEntrega1|TRYBULSKI

---
+++ src/components/paginas/Digitalsport.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Digitalsport = () => {
+    return (
+        <div>
+            <h1>Esta es la página de digital sport</h1>
+        </div>
+    )
+}
+export default Digitalsport
